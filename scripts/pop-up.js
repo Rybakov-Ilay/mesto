dc29b7318c31742a7a9d10194f90b3effd361c32
Rybakov-Ilay <ilay106@mail.ru>
2022-03-29 12:16:40 +0300

    feat: закрывание popup по escape и по overlay

--- scripts/pop-up.js
+++ scripts/pop-up.js
@@ -1,9 +1,7 @@
 const content = document.querySelector(".content");
 const popupEdit = document.querySelector(".popup_edit");
 const forms = document.forms;
-const formList = Array.from(forms);
 const profileEditingForm = forms.profileEditingForm;
-const popupEditButtonClose = popupEdit.querySelector(".popup__button-close");
 const popupEditUserName = profileEditingForm.elements.userName;
 const popupEditUserJob = profileEditingForm.elements.userJob;
 const editButton = content.querySelector(".profile__edit-button");
@@ -12,27 +10,34 @@ const profileUserJob = content.querySelector(".profile__subtitle");
 const popupAddCardButton = content.querySelector(".profile__add-button");
 const popupAdd = document.querySelector(".popup_add");
 const popupAddForm = forms.addCardForm;
-const popupAddCardButtonClose = popupAdd.querySelector(".popup__button-close");
 const cardsList = content.querySelector(".cards__list");
 const popupView = document.querySelector(".popup_view-image");
-const popupViewButtonClose = popupView.querySelector(".popup__button-close");
 const popupViewImage = popupView.querySelector(".popup__image");
 const popupViewImageCaption = popupView.querySelector(".popup__image-caption");
 const cardName = popupAddForm.placeName;
 const cardLink = popupAddForm.placeLink;
+const popupsList = Array.from(document.querySelectorAll(".popup"));
+
+function handlingPopupCloseByEscape(evt) {
+  const activePopup = document.querySelector(".popup_opened");
+  if (evt.key === "Escape") {
+    popupClose(activePopup);
+  }
+}
 
 function popupOpen(popup) {
   popup.classList.add("popup_opened");
+  document.addEventListener("keydown", handlingPopupCloseByEscape);
 }
 
 function popupClose(popup) {
   popup.classList.remove("popup_opened");
+  document.removeEventListener("keydown", handlingPopupCloseByEscape);
 }
 
 function popupEditOpen() {
   popupEditUserName.value = profileUserName.textContent;
   popupEditUserJob.value = profileUserJob.textContent;
-  console.log(profileEditingForm.userName.validity)
   popupOpen(popupEdit);
 }
 
@@ -51,10 +56,19 @@ function handlingPopupAddForm(event) {
   popupAddForm.reset();
 }
 
+function addEventListenerOnCloseAndOverlay(popup) {
+  popup.addEventListener("click", (evt) => {
+    if (
+      evt.target.classList.contains("popup") ||
+      evt.target.classList.contains("popup__button-close")
+    ) {
+      popupClose(popup);
+    }
+  });
+}
+
+popupsList.forEach(addEventListenerOnCloseAndOverlay);
 editButton.addEventListener("click", popupEditOpen);
-popupEditButtonClose.addEventListener("click", () => popupClose(popupEdit));
 profileEditingForm.addEventListener("submit", handlingPopupEditForm);
 popupAddCardButton.addEventListener("click", () => popupOpen(popupAdd));
-popupAddCardButtonClose.addEventListener("click", () => popupClose(popupAdd));
 popupAddForm.addEventListener("submit", handlingPopupAddForm);
-popupViewButtonClose.addEventListener("click", () => popupClose(popupView));
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,31 +1,43 @@
+const config = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__submit",
+  inactiveButtonClass: "popup__submit_inactive",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_active",
+};
 
-
-
-
+// Находим класс ошибки и получаем
+// разметку ошибки соответствующего ей инпута через его id
 const getElementError = (form, element) => {
   return form.querySelector(`.${element.id}-error`);
 };
 
-const showError = (form, element, errorMessage) => {
+const showError = (
+  form,
+  element,
+  errorMessage,
+  { inputErrorClass, errorClass }
+) => {
   const elementError = getElementError(form, element);
 
-  element.classList.add("popup__input_type_error");
+  element.classList.add(inputErrorClass);
   elementError.textContent = errorMessage;
-  elementError.classList.add("popup__input-error_active");
+  elementError.classList.add(errorClass);
 };
 
-const hideError = (form, element, errorMessage) => {
+const hideError = (form, element, { inputErrorClass, errorClass }) => {
   const elementError = getElementError(form, element);
 
-  element.classList.remove("popup__input_type_error");
-  elementError.textContent = errorMessage;
-  elementError.classList.remove("popup__input-error_active");
+  element.classList.remove(inputErrorClass);
+  elementError.textContent = "";
+  elementError.classList.remove(errorClass);
 };
 
-const isValid = (form, element) => {
+const isValid = (form, element, config) => {
   !element.validity.valid
-    ? showError(form, element, element.validationMessage)
-    : hideError(form, element);
+    ? showError(form, element, element.validationMessage, config)
+    : hideError(form, element, config);
 };
 
 const hasInvalidInput = (inputList) => {
@@ -34,36 +46,42 @@ const hasInvalidInput = (inputList) => {
   });
 };
 
-const toggleButtonState = (inputList, button) => {
+const toggleButtonState = (inputList, button, { inactiveButtonClass }) => {
   if (hasInvalidInput(inputList)) {
-    button.classList.add("popup__submit_inactive");
+    button.classList.add(inactiveButtonClass);
     button.setAttribute("disabled", "");
   } else {
-    button.classList.remove("popup__submit_inactive");
+    button.classList.remove(inactiveButtonClass);
     button.removeAttribute("disabled");
   }
 };
 
-const setEventListeners = (form) => {
-  const inputList = Array.from(form.querySelectorAll(".popup__input"));
-  const button = form.querySelector(".popup__submit");
-  toggleButtonState(inputList, button);
+const setEventListeners = (
+  form,
+  { inputSelector, submitButtonSelector, ...config }
+) => {
+  const inputList = Array.from(form.querySelectorAll(inputSelector));
+  const button = form.querySelector(submitButtonSelector);
+
+  toggleButtonState(inputList, button, config);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
-      isValid(form, inputElement);
-      toggleButtonState(inputList, button);
+      isValid(form, inputElement, config);
+      toggleButtonState(inputList, button, config);
     });
   });
 };
 
-const enableValidation = () => {
+const enableValidation = ({ formSelector, ...config }) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
+
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
-    setEventListeners(formElement);
+    setEventListeners(formElement, config);
   });
 };
 
-enableValidation();
+enableValidation(config);
